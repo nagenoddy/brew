8e4eeca05b641af5a01e00b8ae7d2d5c60d66f43
nagenoddy <rory.donegan@gmail.com>
2026-07-13 19:14:21 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='brewguide-premium-v14';
+const CACHE='brewguide-premium-v18';
 const ASSETS=['./','index.html','style.css','recipes.js','app.js','manifest.webmanifest','sw.js','icons/icon-192.png','icons/icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
